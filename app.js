55c1001acfa208c76075e1950dcb588d850552e2
lucapicovici <50777241+lucapicovici@users.noreply.github.com>
2020-04-15 18:23:19 +0300

    Add accommodation model

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-var express = require("express"),
-    app     = express();
+var express  = require("express"),
+    mongoose = require("mongoose"),
+    app      = express();
 
 var indexRoutes         = require("./routes/index.js"),
     accommodationRoutes = require("./routes/accommodation.js");
 
 app.set("view engine", "ejs");
+
+mongoose.set("useUnifiedTopology", true);
+mongoose.set("useNewUrlParser", true);
+mongoose.connect("mongodb://localhost/hotel-go");
 
 app.use("/", indexRoutes);
 app.use("/accommodation", accommodationRoutes);
--- routes/accommodation.js
+++ routes/accommodation.js
@@ -1,11 +1,42 @@
 var express = require("express"),
     router  = express.Router();
 
+var Accommodation = require("../models/accommodation.js");
+
 router.get("/", function(req, res){
     res.render("accommodation/index");
 });
 
 router.get("/:id", function(req, res){
+    Accommodation.create({
+        name: "Hotel Go",
+        type: "Hotel",
+        address: "Markeaton Street",
+        photos: [
+            {src: "https://r-cf.bstatic.com/images/hotel/max1280x900/130/130867441.jpg"},
+            {src: "https://r-cf.bstatic.com/images/hotel/max1280x900/245/245147790.jpg"}
+        ],
+        description: "Hotel description here.",
+        amenities: [
+            "Wi-fi",
+            "Bar"
+        ],
+        rating: 7.6,
+        roomTypes: [
+            {
+                name: "Single room",
+                peopleNo: 1,
+                roomCount: 7,
+                price: 55
+            },
+            {
+                name: "Double room",
+                peopleNo: 2,
+                roomCount: 5,
+                price: 79
+            }
+        ]
+    });
     res.render("accommodation/show");
 });
 
